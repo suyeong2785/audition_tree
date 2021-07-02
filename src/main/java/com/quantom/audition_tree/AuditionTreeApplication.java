3e4c2796ee2afea604d236e9a3b3f4b54b22460c
suyeong2785 <qkdtpwls_@naver.com>
2021-07-02 13:50:31 +0900

    시작

---
+++ src/main/java/com/quantom/audition_tree/AuditionTreeApplication.java
@@ -0,0 +1,13 @@
+package com.quantom.audition_tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditionTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditionTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantom/audition_tree/AuditionTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantom.audition_tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditionTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
